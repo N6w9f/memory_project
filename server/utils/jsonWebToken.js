067fc14c581c88304799fb4620e378a69a9dd61f
N6w9f <158752757+N6w9f@users.noreply.github.com>
2024-05-17 02:32:21 +0300

    Delete server directory

--- server/utils/jsonWebToken.js
+++
@@ -1,6 +0,0 @@
-import jwt from "jsonwebtoken";
-const jwtFn = (object) => {
-    return jwt.sign(object, process.env.SECRET_KEY, { expiresIn: "1d" });
-};
-
-export default jwtFn;
--- server/utils/standards/status.js
+++
@@ -1,5 +0,0 @@
-const SUCCESS = "success";
-const ERROR = "error";
-const FAIL = "fail";
-
-export { SUCCESS, ERROR, FAIL };
